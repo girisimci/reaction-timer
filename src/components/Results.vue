9900aba38e8a6b647e352ebe089ea9780c3f7335
Mehmet Sait Işık <40969473+girisimci@users.noreply.github.com>
2022-01-04 15:11:37 +0300

    game commit

---
+++ src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+  <h2>Reaction Time - {{ score }} ms</h2>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = "Ninja gibisin";
+    } else if (this.score < 400) {
+      this.rank = "Tavşan reflexi...";
+    } else {
+      this.rank = "Kaplumbağa olmadığına emin misin?";
+    }
+  },
+};
+</script>
+
+<style>
+.rank {
+  font-size: 1.4em;
+  color: green;
+  font-weight: bold;
+}
+</style>
